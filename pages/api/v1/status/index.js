397e5abe7caaeb0a8e7a573eb6aefcc1d60add92
cadediego <diegoamaralcamp@gmail.com>
2025-11-01 01:47:52 -0300

    changing database to handle error and costumize parameter queries to safely recieve data

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,30 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("Select 1+1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "São Acima da média" });
+  const UpdateAt = new Date().toISOString();
+
+  const DbName = process.env.POSTGRES_DB;
+
+  const db_version = await database.query("SHOW SERVER_VERSION;");
+  const db_max_connections = await database.query("SHOW max_connections;");
+  const db_open_connections = await database.query({
+    text: "select count(*)::int from pg_stat_activity where datname= $1",
+    values: [DbName],
+  });
+  const OpenConnections = db_open_connections.rows[0].count;
+
+  const MaxConnections = db_max_connections.rows[0].max_connections;
+  const Version = db_version.rows[0].server_version;
+  response.status(200).json({
+    update_date: UpdateAt,
+    dependences: {
+      database: {
+        version: Version,
+        max_connections: parseInt(MaxConnections),
+        open_connections: OpenConnections,
+      },
+    },
+  });
 }
 
 export default status;
