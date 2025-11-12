78fe75a3b1e6dadb5f2dc2ea699491cdc58d83e3
cadediego <diegoamaralcamp@gmail.com>
2025-11-12 00:23:10 -0300

    quick fix

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -26,7 +26,7 @@ export default async function migrations(request, response) {
 
     if (request.method === "GET") {
       const migrations = await migrationsRunner(defaultMigrationOptions);
-      response.status(200).json([migrations]);
+      return response.status(200).json([migrations]);
     }
 
     if (request.method === "POST") {
